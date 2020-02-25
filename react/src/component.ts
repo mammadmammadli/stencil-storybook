01d8e05ae498c4b39bc2e577f3f9b240c6512853
Mammad <mammad.mammadli@pashabank.az>
2020-02-25 22:22:09 +0400

    change package.json

---
+++ react/src/component.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from 'pashalib';
+
+import { defineCustomElements, applyPolyfills } from 'pashalib/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const UiButton = /*@__PURE__*/createReactComponent<JSX.UiButton, HTMLUiButtonElement>('ui-button');
