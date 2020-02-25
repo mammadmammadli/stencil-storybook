06a89ba65b38d1af77993797d06be3ef0605f752
Mammad <mammad.mammadli@pashabank.az>
2020-02-25 22:22:23 +0400

    implement reactoutput

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,10 +1,14 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
-// import { reactOutputTarget } from '@stencil/react-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'lib',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'pashalib',
+      proxiesFile: 'react/src/component.ts'
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
