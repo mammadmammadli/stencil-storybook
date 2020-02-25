6cc22a84f2b7678079c09018c7de00fd12cf931f
Mammad <mammad.mammadli@pashabank.az>
2020-02-25 22:02:13 +0400

    implement react build

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,14 +1,10 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
-import { reactOutputTarget } from '@stencil/react-output-target';
+// import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'lib',
   outputTargets: [
-    reactOutputTarget({
-      componentCorePackage: 'uilib-react',
-      proxiesFile: 'dist/react/components.ts',
-    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
